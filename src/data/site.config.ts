91afb0a72719d4d6eac92fbdbb4bd80f91cbc092
Francesco Mollica <38285976+francimolli@users.noreply.github.com>
2024-11-02 03:28:54 +0100

    first release

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	author: 'spank',
+	title: 'pay in bitcoin',
+	description: 'un piccolo blog fatto su misura per creare una piccola comunità di bitcoiners',
+	lang: 'it-IT',
+	ogLocale: 'it_IT',
+	shareMessage: 'orangpillare',
 	paginationSize: 6 // Number of posts per page
 }
