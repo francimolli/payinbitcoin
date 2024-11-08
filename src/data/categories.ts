5750dac07baa39ac12b12bacb187b3b9ed8da28f
Francesco Mollica <38285976+francimolli@users.noreply.github.com>
2024-11-08 10:01:00 +0100

    Adding new category

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,2 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Iniziare', 'Acquistare', 'Conservare', 'Programmare'] as const
+export const CATEGORIES = ['Iniziare', 'Acquisire', 'Conservare', 'Programmare'] as const
